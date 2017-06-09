03cd573e3a8182558538a1c133856422b25a2101
Antò <aro@einfachsicher.ch>
2017-06-09 21:31:13 +0200

    add config testing with mocha

---
+++ test/stringUtils.spec.js
@@ -0,0 +1,10 @@
+var expect    = require('chai').expect;
+var stringUtils = require('../util/stringUtils');
+
+describe('StringUtils class testing', function () {
+
+    it('should return true', function () {
+        expect(true).to.equal(true);
+    });
+
+});
