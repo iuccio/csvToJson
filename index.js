f6bad28a7bb10c3fdb85ba46dacacac2f4456222
Antò <aro@einfachsicher.ch>
2017-06-09 14:00:03 +0200

    add API back compatibility for version 0.0.3

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 'use strict';
 
-let csvToJson = require('./src/csvToJson.js');
-
-/*
+/**
  * Parse .csv file and put its content into a file in json format.
+ * @param {inputFileName} path/filename
+ * @param {outputFileName} path/filename
+ *
  */
+let csvToJson = require('./src/csvToJson.js');
 exports.generateJsonFileFromCsv = function (inputFileName, outputFileName) {
     csvToJson.generateJsonFileFromCsv(inputFileName, outputFileName);
 };
 
-/*
+/**
  * Parse .csv file and put its content into an Array of Object in json format.
+ * @param {inputFileName} path/filename
+ * @return {Array} Array of Object in json format
+ *
  */
 exports.getJsonFromCsv = function (inputFileName) {
     return csvToJson.getJsonFromCsv(inputFileName);
+};
+
+/**
+ * Parse .csv file and put its content into a file in json format.
+ * @param {inputFileName} path/filename
+ * @param {outputFileName} path/filename
+ *
+ * @deprecated Use generateJsonFileFromCsv()
+ */
+exports.jsonToCsv = function (inputFileName, outputFileName) {
+    csvToJson.generateJsonFileFromCsv(inputFileName, outputFileName);
 };
