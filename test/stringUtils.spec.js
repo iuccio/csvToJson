66f712af979c3a8c4827632515e5320bde5bf056
Antò <aro@einfachsicher.ch>
2017-06-09 22:35:40 +0200

    add tests for stringUtils class

--- test/stringUtils.spec.js
+++ test/stringUtils.spec.js
@@ -5,15 +5,57 @@ let stringUtils = require('../src/util/stringUtils');
 
 describe('StringUtils class testing', function () {
 
-    it('Should trim input value', function () {
-        //given
-        let value = 'value ';
+    describe('trimPropertyName()', function () {
 
-        //when
-        let result = stringUtils.trimPropertyName(value);
+        it('Should trim input value', function () {
+            //given
+            let value = ' value ';
 
-        //then
-        expect(result).to.equal('value');
+            //when
+            let result = stringUtils.trimPropertyName(value);
+
+            //then
+            expect(result).to.equal('value');
+        });
     });
+
+    describe('getValueFormatByType()', function () {
+        it('should return type of Number', function () {
+            //given
+            let value = '23';
+
+            //when
+            let result = stringUtils.getValueFormatByType(value);
+
+            //then
+            expect(result).to.be.an('number');
+            expect(result).to.equal(23);
+        });
+
+        it('should return type of String when value contains only words', function () {
+            //given
+            let value = 'value';
+
+            //when
+            let result = stringUtils.getValueFormatByType(value);
+
+            //then
+            expect(result).to.be.an('string');
+            expect(result).to.equal('value');
+        });
+
+        it('should return type of String when value contains words and digits', function () {
+            //given
+            let value = '11value';
+
+            //when
+            let result = stringUtils.getValueFormatByType(value);
+
+            //then
+            expect(result).to.be.an('string');
+            expect(result).to.equal('11value');
+        });
+    });
+
 
 });
