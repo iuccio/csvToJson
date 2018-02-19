d838dc74ee1f489300a22f6c8e8e0e5c35f94859
Eric Pan <epan@users.noreply.github.com>
2018-02-19 14:09:51 -0800

    (refactor) Small formatting consistency

--- src/util/stringUtils.js
+++ src/util/stringUtils.js
@@ -14,10 +14,10 @@ class StringUtils {
         return String(value);
     }
 
-    hasContent(values){
-        if(values.length >0){
-            for(let i=0; i<values.length;i++){
-                if(values[i]){
+    hasContent(values) {
+        if (values.length > 0) {
+            for (let i = 0; i < values.length; i++) {
+                if (values[i]) {
                     return true;
                 }
             }
