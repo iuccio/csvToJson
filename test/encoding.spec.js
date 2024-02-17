c9c3b579b16d610d03508132877de8906226a0e2
Antò <aro@einfachsicher.ch>
2024-02-17 16:58:30 +0000

    WIP migrate to jest

--- test/encoding.spec.js
+++ test/encoding.spec.js
@@ -1,7 +1,4 @@
 'use strict';
-
-let expect = require('chai').expect;
-let assert = require('chai').assert;
 let index = require('../index');
 
 describe('Encoding testing', function () {
@@ -11,9 +8,8 @@ describe('Encoding testing', function () {
         let result = index.latin1Encoding().getJsonFromCsv('test/resource/input_latin1_encode.csv');
 
         //then
-        assert.isNotEmpty(result);
-        expect(result[0].l_ATC1).to.be.equal('Système digestif et métabolisme');
-        expect(result[0]).to.deep.include({l_ATC1: 'Système digestif et métabolisme'});
+        expect(result).not.toBeNull()
+        expect(result[0].l_ATC1).toEqual('Système digestif et métabolisme');
     });
 
 });
