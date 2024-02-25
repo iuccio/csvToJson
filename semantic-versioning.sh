662367d3cfd4eac14d341b5252f57600d18847d2
Antò <aro@einfachsicher.ch>
2024-02-25 11:44:19 +0000

    update ci/cd

--- semantic-versioning.sh
+++ semantic-versioning.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+
 set -e
-echo "Start Semantic Versioning release...";
+
+validate_is_master_branch(){
 echo "Checking branch...";
 RELEASE_BRANCH="master";
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD);
@@ -9,6 +11,9 @@ if [ $RELEASE_BRANCH != $CURRENT_BRANCH ]; then
     echo "Exiting...";
     exit 0;
 fi
+}
+
+npm_release(){
 PATCH_MSG="[PATCH]";
 MAJOR_MSG="[MAJOR]";
 echo "Parsing git message...";
@@ -16,12 +21,33 @@ COMMIT_MSG=$(git log -1 --pretty=format:"%s");
 echo "Last commit message: ${COMMIT_MSG}";
 if [[ $COMMIT_MSG == *"$PATCH_MSG"* ]]; then
     echo "Executing new PATCH release..."
-    npm run version-patch;
+    npm version patch --force;
 elif [[ $COMMIT_MSG == *"$MAJOR_MSG"* ]]; then
     echo "Executing new MAJOR release..."
-    npm run version-major;
+    npm version major --force;
 else
   echo "Executing new MINOR release...";
-  npm run version-minor;
+  npm version minor --force;
 fi
-echo "End Semantic Versioning release.";
+echo "Publish new version..."
+##npm publish;
+echo "New version successfully published."
+}
+
+push_git_info(){
+  echo "Publish git info...";
+  TAG=$(git describe --tags --abbrev=0)
+  git commit --amend -m "new release $TAG [skip ci]"
+  echo "new commit message: $(git log -1 --pretty=format:"%s")"
+  ##git push --follow-tags
+}
+
+execute-new-release(){
+    echo "Start Semantic Versioning release...";
+    validate_is_master_branch;
+    npm_release;
+    push_git_info;
+    echo "End Semantic Versioning release.";
+}
+
+execute-new-release;
