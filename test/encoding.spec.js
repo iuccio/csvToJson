036043ee62f22bd97c4a66a4a59edf6261bba689
Aras Abbasi <aras.abbasi@googlemail.com>
2025-10-04 10:45:16 +0200

    Remove jest (#1)
    * chore: replace jest with node own test runner, with fallback solution
    
    * runn tests on pull_request
    
    * smarter
    
    * set fail-fast to false

--- test/encoding.spec.js
+++ test/encoding.spec.js
@@ -1,17 +1,15 @@
 'use strict';
-let index = require('../index');
+
+const { describe, it } = require('./test');
+const index = require('../index');
 
 describe('Encoding testing', function () {
-
-    it('should return an object with latin1 encode decoded', function () {
+    it('should return an object with latin1 encode decoded', function (t) {
         //when
         let result = index.latin1Encoding().getJsonFromCsv('test/resource/input_latin1_encode.csv');
 
         //then
-        expect(result).not.toBeNull()
-        expect(result[0].l_ATC1).toEqual('Système digestif et métabolisme');
+        t.assert.notStrictEqual(result, null);
+        t.assert.strictEqual(result[0].l_ATC1, 'Système digestif et métabolisme');
     });
-
 });
-
-
